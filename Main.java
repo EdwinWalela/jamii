8f6c6f026dcfe896fc58bafbedbd5eefa3d5ab58
edwinwalela <edwinwalela@gmail.com>
2020-07-29 11:15:12 +0300

    delete BC key generator

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
 package com.company;
 
-import com.company.crypto.ECBC;
-import com.company.primitives.Block;
 import com.company.primitives.Chain;
-import com.company.primitives.Transaction;
 import com.company.primitives.Wallet;
-import io.github.novacrypto.bip39.MnemonicGenerator;
-import io.github.novacrypto.bip39.Words;
-import io.github.novacrypto.bip39.wordlists.English;
-import org.apache.commons.codec.DecoderException;
-import org.web3j.crypto.Sign;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.*;
-import java.security.spec.InvalidKeySpecException;
 
 public class Main {
 
