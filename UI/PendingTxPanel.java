a4ed6eebd49a6b086ec06cd814e7da1fe991abeb
edwinwalela <edwinwalela@gmail.com>
2020-09-12 22:58:18 +0300

    pending tx arrangement

--- UI/PendingTxPanel.java
+++ UI/PendingTxPanel.java
@@ -14,10 +14,6 @@ public class PendingTxPanel extends JPanel {
 
     List<Transaction> pendingTxs;
 
-    JLabel fromHeader = new JLabel("From");
-    JLabel toHeader = new JLabel("Target");
-    JLabel signatureHeader = new JLabel("Signature");
-
     JButton mineBtn = new JButton("Verify Pending Transactions");
     JButton refreshBtn = new JButton("Refresh");
 
@@ -25,26 +21,17 @@ public class PendingTxPanel extends JPanel {
         pendingTxs = _pendingTxs;
         Font font = new Font("San Serif",Font.PLAIN,13);
 
-        fromHeader.setBounds(20,0,100,100);
-        toHeader.setBounds(160, 0, 100,100);
-        signatureHeader.setBounds(300,0,100,100);
         mineBtn.setBounds(50,520,200,30);
         refreshBtn.setBounds(270,520,80,30);
 
         mineBtn.setFont(font);
         refreshBtn.setFont(font);
-        fromHeader.setFont(font);
-        toHeader.setFont(font);
-        signatureHeader.setFont(font);
 
-        add(fromHeader);
-        add(toHeader);
-        add(signatureHeader);
         add(mineBtn);
         add(refreshBtn);
 
 
-        setBorder(new TitledBorder(new LineBorder(Color.lightGray,2),"Pending Votes"));
+        setBorder(new TitledBorder(new LineBorder(Color.lightGray,2),"Pending Transactions"));
         setLayout(null);
         setVisible(true);
     }
@@ -55,33 +42,28 @@ public class PendingTxPanel extends JPanel {
 
     public void buildTxList(){
         int size = pendingTxs.size();
-        int y = 20;
+        int y = 0;
         for(int i = 0; i<size; i++){
             Transaction tx = pendingTxs.get(i);
 
-            String fromAddress = tx.getFrom();
-            String toAddress = tx.getTarget();
+            JLabel from = new JLabel("From: "+tx.getFrom());
+            JLabel to = new JLabel("Target: "+tx.getTarget());
+            JLabel signature = new JLabel("Signature: "+tx.getSignature());
+            JLabel tx_hash = new JLabel("Hash: "+tx.getHash());
+            JSeparator separator = new JSeparator();
 
-            String fromHead = fromAddress.substring(0,3);
-            String fromTail = fromAddress.substring(fromAddress.length()-3,fromAddress.length());
-
-            String toHead = toAddress.substring(0,3);
-            String toTail = toAddress.substring(toAddress.length()-3,toAddress.length());
-
-            JLabel from = new JLabel(fromHead+"..."+fromTail);
-            JLabel to = new JLabel(toHead+"..."+toTail);
-            JLabel signature = new JLabel(tx.getSignature());
-
-
-
-            from.setBounds(20,y+50,60,50);
-            to.setBounds(140, y+50 , 60,50);
-            signature.setBounds(260,y+50,60,50);
+            from.setBounds(20,y+30,350,50);
+            to.setBounds(20, y+55 , 350,50);
+            signature.setBounds(20,y+80,350,50);
+            tx_hash.setBounds(20,y+105,350,50);
+            separator.setBounds(20,y+150,350,50);
 
             add(from);
             add(to);
             add(signature);
-            y+=20;
+            add(tx_hash);
+            add(separator);
+            y+=120;
         }
         revalidate();
         repaint();
